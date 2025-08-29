ae089b7c2f2684b4b69e32eb363e8e67b69b296e
Awais-niaiz <af514325@gmail.com>
2025-08-29 06:35:19 +0500

    prepare for railway

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -93,17 +93,18 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 CONN_MAX_AGE = config('CONN_MAX_AGE', cast=int, default=30)
 DATABASE_URL = config("DATABASE_URL", default=None)
 
+# Override with production database if DATABASE_URL is provided
 if DATABASE_URL is not None:
     import dj_database_url
     DATABASES = {
-       'default': dj_database_url.config(
-           default=DATABASE_URL,
-           conn_max_age=CONN_MAX_AGE,
-           conn_health_checks=True,
-           ) 
+        'default': dj_database_url.config(
+            conn_max_age=CONN_MAX_AGE,
+            conn_health_checks=True,
+        )
     }
 
 
